1adbceec7d251bee0e39efecb67805fa20af7fde
ixmo <ixmo@fresnou.org>
2023-03-14 13:55:49 -0700

    initial commit

---
+++ functionnotes.js
@@ -0,0 +1,44 @@
+//Function Notes
+//Step 1 - Define the Function
+// function sayHello() {
+//     console.log('Hello');
+// }
+//Step 2 - Call (use) the function
+// sayHello();
+// sayHello();
+// sayHello();
+// sayHello();
+
+// function printMax() {
+//     let x = 6;
+//     let y = 29;
+//     if (x>y){
+//         console.log(`The larger number is ${x}`);
+//     } else {
+//         console.log(`The larger number is ${y}`);
+//     }
+// }
+//
+// printMax();
+
+// function printMax(x, y) {
+//     if (x>y){
+//         console.log(`The larger number is ${x}`);
+//     } else {
+//         console.log(`The larger number is ${y}`);
+//     }
+// }
+//
+// printMax(8, 1231);
+// printMax(2, -21);
+// printMax(456, 24561);
+// printMax(2, -21);
+
+function sum(number1, number2){
+    console.log(`${number1} + ${number2} = ${number1+number2}`)
+}
+
+sum(8, 1231);
+sum(2, -21);
+sum(456, 24561);
+sum(2, -21);

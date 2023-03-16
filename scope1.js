75e185c51fed1e90f3e925decdbea4588c3abfe9
Dreftal91 <ixmo@fresnou.org>
2023-03-16 10:28:10 -0700

    Started scope exercises

---
+++ scope1.js
@@ -0,0 +1,27 @@
+//number 1
+
+function nameSwapper(first, last) {
+    console.log(last);
+    console.log(first);
+}
+
+nameSwapper(Efren, Torres);
+
+//number 2
+
+function numberGenerator(num1, num2) {
+    let total = num1 * 10 + num2;
+    console.log(total)
+}
+
+numberGenerator(1, 7)
+
+//number 3
+
+function averageTemperature(temp1, temp2, temp3, temp4, temp5, temp6, temp7) {
+    let Atemp1 = temp1+temp2+temp3+temp4+temp5+temp6+temp7;
+    let Atempfinal = Atemp1/7;
+    console.log(Atempfinal);
+}
+
+averageTemperature(67, 34, 23, 124, 234 ,234, 34)

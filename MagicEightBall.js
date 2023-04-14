049ad0d759886251b87b5257904e0cbc62eeee06
ixmo <ixmo@fresnou.org>
2023-04-14 14:19:09 -0700

    Finished magic8ball

--- MagicEightBall.js
+++ MagicEightBall.js
@@ -2,23 +2,26 @@ const readLine = require('readline-sync');
 
 let answers = ['Of course!','Yes', 'No', 'Not in a million years', 'Maybe', 'I am uncertain', 'Most Likely', 'Theoretically, yes', 'Theoretically, no'];
 
-let randomNum = Math.floor(Math.random()*answers.length);
-
-
 console.log('Welcome, you may ask a yes or no question.\nIf you want to quit, type "I want to quit"');
 
+let playtime = 0;
 while (answers != 0) {
-    let question = readLine.question('Ask away: ');
-    let answer = answers[randomNum];
-    console.log(answer);
-    if (question == 0) {
-        question = readLine.question('Ask away: ');
+    for (let i = 0; i != -1; i++) {
+        let question = readLine.question('Ask away: ');
+        let answer = answers[getRandomInt()];
         console.log(answer);
-    }
-    if (question == 'I want to quit') {
-        console.log('Too bad...');
-        return
+        function getRandomInt() {
+            return Math.floor(Math.random() * answers.length);
+        }
+        if (question == 0) {
+            question = readLine.question('Ask away: ');
+            console.log(answer);
+        }
+        playtime++;
+        if (question == 'I want to quit') {
+            console.log('Too bad...');
+            console.log(`You asked the machine ${playtime} times.`);
+            return
+        }
     }
 }
-
-console.log(answer);
